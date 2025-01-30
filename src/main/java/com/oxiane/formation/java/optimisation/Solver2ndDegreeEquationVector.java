7298886706bb042f6f332a76b3851835e4262c49
Christophe Marchand <christophe@marchand.top>
2025-01-30 16:48:15 +0100

    inline de vectors pour éviter des déclarations

--- src/main/java/com/oxiane/formation/java/optimisation/Solver2ndDegreeEquationVector.java
+++ src/main/java/com/oxiane/formation/java/optimisation/Solver2ndDegreeEquationVector.java
@@ -24,29 +24,30 @@ public class Solver2ndDegreeEquationVector implements Solver2ndDegreeEquation {
       cs[i] = equation.c();
       solutions[i] = new EquationSolution(equation);
     }
-    int index = 0;
-    double[] discriminents = new double[as.length];
+    double[] discriminants = new double[as.length];
     double[] roots1 = new double[as.length];
     double[] roots2 = new double[as.length];
+    int index = 0;
     for(; index < species.loopBound(as.length); index += species.length()) {
       DoubleVector A = DoubleVector.fromArray(species, as, index);
       DoubleVector B = DoubleVector.fromArray(species, bs, index);
       DoubleVector C = DoubleVector.fromArray(species, cs, index);
-      DoubleVector squareB = B.mul(B);
-      DoubleVector ac4 = A.mul(4d)
-                          .mul(C);
-      DoubleVector DISCRIMINENT = squareB.sub(ac4);
-      DISCRIMINENT.intoArray(discriminents, index);
-      // traitement des discriminents 0
-      VectorMask<Double> nullDiscriminents = DISCRIMINENT.compare(VectorOperators.EQ, 0d);
+      DoubleVector DISCRIMINANT = B
+          .mul(B)
+          .sub(A
+              .mul(4d)
+              .mul(C));
+      DISCRIMINANT.intoArray(discriminants, index);
+      // traitement des discriminants 0
+      VectorMask<Double> nullDiscriminents = DISCRIMINANT.compare(VectorOperators.EQ, 0d);
       DoubleVector ROOT1 = B
           .mul(-1d, nullDiscriminents)
           .div(2d)
           .div(A, nullDiscriminents);
       ROOT1.intoArray(roots1, index, nullDiscriminents);
-      // traitement des discriminents positifs
-      VectorMask<Double> positiveDiscriminents = DISCRIMINENT.compare(VectorOperators.GT, 0d);
-      DoubleVector sqrt = DISCRIMINENT.lanewise(VectorOperators.SQRT, positiveDiscriminents);
+      // traitement des discriminants positifs
+      VectorMask<Double> positiveDiscriminents = DISCRIMINANT.compare(VectorOperators.GT, 0d);
+      DoubleVector sqrt = DISCRIMINANT.lanewise(VectorOperators.SQRT, positiveDiscriminents);
       B
           .mul(-1d, positiveDiscriminents)
           .add(sqrt.mul(-1d))
@@ -61,23 +62,24 @@ public class Solver2ndDegreeEquationVector implements Solver2ndDegreeEquation {
           .intoArray(roots2, index, positiveDiscriminents);
     }
     for(int index2 = index; index2 < as.length ; index2++) {
-      discriminents[index2] = bs[index2] * bs[index2] - (4 * as[index2] * cs[index2]);
-      if(discriminents[index2] == 0) {
-        // cas des discriminents 0
+      System.out.println("passage dans correctif de longueur");
+      discriminants[index2] = bs[index2] * bs[index2] - (4 * as[index2] * cs[index2]);
+      if(discriminants[index2] == 0) {
+        // cas des discriminants 0
         roots1[index2] = (- bs[index2])/(2 + as[index2]);
-      } else if(discriminents[index2] > 0) {
-        // cas des discriminents > 0
-        double sqrt = Math.sqrt(discriminents[index2]);
+      } else if(discriminants[index2] > 0) {
+        // cas des discriminants > 0
+        double sqrt = Math.sqrt(discriminants[index2]);
         roots1[index2] = (- bs[index2] - sqrt)/(2 + as[index2]);
         roots2[index2] = (- bs[index2] + sqrt)/(2 + as[index2]);
       }
     }
-    for (int i = 0; i < discriminents.length; i++) {
-      solutions[i].setDiscriminent(discriminents[i]);
-      if(discriminents[i] >= 0) {
+    for (int i = 0; i < discriminants.length; i++) {
+      solutions[i].setDiscriminent(discriminants[i]);
+      if(discriminants[i] >= 0) {
         solutions[i].addSolution(roots1[i]);
       }
-      if(discriminents[i] > 0) {
+      if(discriminants[i] > 0) {
         solutions[i].addSolution(roots2[i]);
       }
     }
