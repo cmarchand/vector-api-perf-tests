8fdfab1d57bdb24a251137f9f0cbb8082f435a1c
Christophe Marchand <christophe@marchand.top>
2025-02-20 12:23:29 +0100

    ajout calcul taille stream float

--- src/main/java/com/oxiane/formation/java/optimisation/Main.java
+++ src/main/java/com/oxiane/formation/java/optimisation/Main.java
@@ -4,13 +4,16 @@ import com.oxiane.formation.java.optimisation.doubles.EquationDouble;
 import com.oxiane.formation.java.optimisation.doubles.EquationSolutionDouble;
 import com.oxiane.formation.java.optimisation.doubles.Solver2NdDegreeEquationDoubleRegular;
 import jdk.incubator.vector.DoubleVector;
+import jdk.incubator.vector.FloatVector;
 import jdk.incubator.vector.VectorSpecies;
 
 public class Main {
   public static void main(String[] args) {
     // 4/3 x^2 + 5x + 3 = 0
-    VectorSpecies<Double> species = DoubleVector.SPECIES_PREFERRED;
-    System.out.println("Proc stream length: " + species.length()*64+"bits");
+    VectorSpecies<Double> speciesDouble = DoubleVector.SPECIES_PREFERRED;
+    System.out.println("Proc stream length: " + speciesDouble.length()*Double.BYTES*8+"bits");
+    VectorSpecies<Float> speciesFloat = FloatVector.SPECIES_PREFERRED;
+    System.out.println("Proc stream length: " + speciesFloat.length()*Float.BYTES*8+"bits");
     EquationSolutionDouble solution = Solver2NdDegreeEquationDoubleRegular.solve(new EquationDouble(4d/3d, 5d, 3d));
     System.out.println(solution);
   }
